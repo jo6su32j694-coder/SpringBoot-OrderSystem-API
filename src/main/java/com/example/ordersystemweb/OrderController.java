5cb6b43846f97ea1b98ecd82813bf0db5be78323
Jeff <jo6su32j694@gmail.com>
2026-09-19 00:37:28 +0800

    feat:實作點單扣庫存的POST API功能

--- src/main/java/com/example/ordersystemweb/OrderController.java
+++ src/main/java/com/example/ordersystemweb/OrderController.java
@@ -1,7 +1,9 @@
 package com.example.ordersystemweb;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
@@ -30,5 +32,34 @@ public class OrderController {
         products.add(friedChicken);
         products.add(sausage);
         return products;
+    }
+
+    //新增：處理點單購買的POST API
+    //網址將會是"/api/order-system/buy?itemname=奶茶"
+    @PostMapping("/buy")
+    public String buyItem(@RequestParam String itemName){
+
+        //1.根據前端傳過來的商品名稱，尋找對應的商品物件
+        Products targetProduct = null;
+        if(milkTea.getName().equals(itemName)){
+            targetProduct = milkTea;
+        }else if(friedChicken.getName().equals(itemName)){
+            targetProduct = friedChicken;
+        }else if(sausage.getName().equals(itemName)){
+            targetProduct = sausage;
+        }
+
+        //2.檢查有沒有找到商品
+        if (targetProduct == null){
+            return "錯誤：找不到名為「"+itemName+"」的商品！";
+        }
+
+        //3.檢查庫存並扣除
+        if (targetProduct.getStock()>0){
+            targetProduct.setStock(targetProduct.getStock()-1);//扣除1個庫存
+            return "購買成功！「"+targetProduct.getName()+"」已加入購物車，剩餘庫存： "+targetProduct.getStock();
+        }else{
+            return "失敗：商品「"+targetProduct.getName()+"」已售罄，無法購買！";
+        }
     }
 }
